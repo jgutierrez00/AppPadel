7303c6e7e8192f601ce8ea5bbaf7f8d3f33267ed
jgutierrez00 <josemariagutierrez@uma.es>
2021-08-20 23:15:31 +0200

    Fix

--- website/views.py
+++ website/views.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
-from . import db
 
 
 views = Blueprint('views', __name__)
 
+horas = [['10:00-11:30', 'Hola'], ['11:30-13:00', 'Libre'], ['15:00-16:30', 'Libre'], ['16:30-18:00', 'Libre'], 
+    ['18:00-19:30', 'Libre'], ['19:30-21:00', 'Libre'], ['21:00-22:30', 'Libre']]
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    horas = [['10:00-11:30', ''], ['11:30-13:00', ''], ['15:00-16:30', ''], ['16:30-18:00', ''], ['18:00-19:30', ''], ['19:30-21:00', ''], ['21:00-22:30', '']]
+    if request.method == 'POST':
+        if request.form.get('btn'):
+            print(request.form.get('btn'))
+
     return render_template("home.html", user=current_user, horas=horas)
