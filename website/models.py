b2e61cf203fe12092b5ce7e5e8a08dfc185c0d59
jgutierrez00 <josemariagutierrez@uma.es>
2021-08-20 15:30:15 +0200

    Database model created

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Information(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    horaReserva = db.Column(db.String(128), unique=True)
+    horaFinalizacion = db.Column(db.String(128), unique=True)
+    ip = db.Column(db.String(128), unique=True)
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(128))
+    apellido1 = db.Column(db.String(128))
+    apellido2 = db.Column(db.String(128))
+    dni = db.Column(db.String(10), unique=True)
+    contraseña = db.Column(db.String(128))
+    information = db.relationship('Information')
