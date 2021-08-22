df89fef82300612b1337c3865d3af9eee96f1da3
jgutierrez00 <josemariagutierrez@uma.es>
2021-08-22 18:16:42 +0200

    Some updates.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from website.models import User, Information
-from flask import Blueprint, render_template, request, flash
-from sqlalchemy import update
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from flask_login import login_required, current_user
 from . import db
 
@@ -15,26 +14,41 @@ horasp2 = [['10:00-11:30', 'Libre'], ['11:30-13:00', 'Libre'], ['15:00-16:30', '
 dias = [['Lunes', horasp1, horasp2], ['Martes', horasp1, horasp2], ['Miercoles', horasp1, horasp2], 
 ['Jueves', horasp1, horasp2],['Viernes', horasp1, horasp2], ['Sabado', horasp1, horasp2], ['Domingo', horasp1, horasp2]]
 
-diaselect = None
-
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
         if request.form.get('btnday'):
-            diaselect = request.form.get('btnday')
-            return render_template("horarios.html", user=current_user, horasp1=horasp1, horasp2=horasp2)
+            session['diaselect'] = request.form.get('btnday')
+            session['user'] = current_user
+            return redirect(url_for('views.horarios'))
 
     return render_template("home.html", dias=dias, user=current_user)
 
 @views.route('/horarios', methods=['GET', 'POST'])
 @login_required
 def horarios():
+    diaselect = session.get('diaselect',None)
+    if request.method == 'POST':
+        if request.form.get('btn1'):
+            anyadirReserva(current_user.nombre, dias[diaselect][1], request.form.get('btn1'), str(current_user)[6:7], diaselect, 1)
+        elif request.form.get('btn2'):
+            anyadirReserva(current_user.nombre, dias[diaselect][2], request.form.get('btn2'), str(current_user)[6:7], diaselect, 2)
+        elif request.form.get('mbtn1'):
+            eliminarReserva(current_user.nombre, diaselect, 1)
+            anyadirReserva(current_user.nombre, dias[diaselect][1], request.form.get('mbtn1'), str(current_user)[6:7], diaselect, 1)
+        elif request.form.get('mbtn2'):
+            eliminarReserva(current_user.nombre, diaselect, 2)
+            anyadirReserva(current_user.nombre, dias[diaselect][2], request.form.get('mbtn2'), str(current_user)[6:7], diaselect, 2)
+        elif request.form.get('cbtn1'):
+            eliminarReserva(current_user.nombre, dias[diaselect][1], diaselect, 1)
+        elif request.form.get('cbtn2'):
+            eliminarReserva(current_user.nombre, dias[diaselect][2], diaselect, 2)         
+    
+    return render_template("horarios.html", user=current_user, dias1=dias[int(diaselect)][1], dias2=dias[int(diaselect)][2])
 
-    return render_template("horarios.html", user=current_user, horasp1=horasp1, horasp2=horasp2)
-
-def eliminarReserva(nombre):
+def eliminarReserva(nombre, horas, dia, pista):
     exit = False
     cont = 0
     while exit == False and cont < len(horas):
@@ -46,10 +60,11 @@ def eliminarReserva(nombre):
     if exit == False:
         flash('Usted no ha realizado ninguna reserva', category='error')
     else:
+        dias[dia][pista] = horas
         flash('Reserva eliminada con exito', category='success')
             
 
-def anyadirReserva(nombre, index, id):
+def anyadirReserva(nombre, horas, index, id, dia, pista):
     exit = False
     cont = 0
     idx = int(index)
@@ -60,24 +75,17 @@ def anyadirReserva(nombre, index, id):
         cont = cont + 1
     if exit == False:
         horas[idx][1] = nombre
+        dias[dia][pista] = horas
 
-        #PENDIENTE  
-        users = User.query.filter_by(id=id)
-        for user in users:
-            user.reserva = 1
-        db.session.commit()
-        flash('Reserva realizada con exito', category='success')
+        # #PENDIENTE  
+        # users = User.query.filter_by(id=id)
+        # for user in users:
+        #     user.reserva = 1
+        # db.session.commit()
+        # flash('Reserva realizada con exito', category='success')
     else:
         flash('Usted ya ha realizado una reserva. Actualmente solo puede borrar su reserva o modificarla', category='error')
 
         
 
-    # if request.form.get('mbtn'):
-    #         eliminarReserva(current_user.nombre)
-    #         anyadirReserva(current_user.nombre, request.form.get('mbtn'), str(current_user)[6:7])
-    #     elif request.form.get('btn'):
-    #         anyadirReserva(current_user.nombre, request.form.get('btn'), str(current_user)[6:7])
-    #     elif request.form.get('modifybtn'):
-    #         return render_template('modify.html', user=current_user, horas=horas)
-    #     elif request.form.get('deletebtn'):
-    #         eliminarReserva(current_user.nombre)
+    
