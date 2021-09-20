1c125b07da879de17c2f92d8b35afc7b0f696cec
jgutierrez00 <josemariagutierrez@uma.es>
2021-09-20 12:51:03 +0200

    Added change color when book on timetable

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -1,28 +1,7 @@
-function fadeFlash(){
-    $(window).load(function(){
-        setTimeout(function(){
-            $(".alert-success").fadeOut()
-        }, 5000);
-    });
-}
+$(function() {
+  $('#flash').delay(500).fadeIn('normal', function() {
+     $(this).delay(2500).fadeOut();
+  });
+});
 
-(function ($) {
-    'use strict';
-    $('.column100').on('mouseover', function () {
-      var table1 = $(this).parent().parent().parent();
-      var table2 = $(this).parent().parent();
-      var verTable = $(table1).data('vertable') + '';
-      var column = $(this).data('column') + '';
-      $(table2).find('.' + column).addClass('hov-column-' + verTable);
-      $(table1).find('.row100.head .' + column).addClass('hov-column-head-' + verTable);
-    });
-    $('.column100').on('mouseout', function () {
-      var table1 = $(this).parent().parent().parent();
-      var table2 = $(this).parent().parent();
-      var verTable = $(table1).data('vertable') + '';
-      var column = $(this).data('column') + '';
-      $(table2).find('.' + column).removeClass('hov-column-' + verTable);
-      $(table1).find('.row100.head .' + column).removeClass('hov-column-head-' + verTable);
-    });
-  }) (jQuery);
   
