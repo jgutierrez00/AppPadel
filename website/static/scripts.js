60ac02e3eb5054566d38c1627bfd5fe54e7783c0
jgutierrez00 <josemariagutierrez@uma.es>
2021-09-21 12:47:56 +0200

    Refactor timetable page

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -4,4 +4,24 @@ $(function() {
   });
 });
 
+document.getElementById('divbtn1').addEventListener('click', function(){
+  document.getElementById('divbtn1').style.display = 'none';
+  document.getElementById('divbtn2').style.display = 'none';
+  document.getElementById('divttable1').style.display = 'block';
+  document.getElementById('btnbacktt').style.display = 'block';
+})
   
+document.getElementById('divbtn2').addEventListener('click', function(){
+  document.getElementById('divbtn1').style.display = 'none';
+  document.getElementById('divbtn2').style.display = 'none';
+  document.getElementById('divttable2').style.display = 'block';
+  document.getElementById('btnbacktt').style.display = 'block';
+})
+
+document.getElementById('btnbacktt').addEventListener('click', function(){
+  document.getElementById('btnbacktt').style.display = 'none';
+  document.getElementById('divbtn1').style.display = 'block';
+  document.getElementById('divbtn2').style.display = 'block';
+  document.getElementById('divttable1').style.display = 'none';
+  document.getElementById('divttable2').style.display = 'none';
+})
