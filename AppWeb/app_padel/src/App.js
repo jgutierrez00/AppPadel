c5e7dbaa5dcc4505acd4cf1d56b0661f141a1fc5
jgutierrez00 <josemariagutierrez@uma.es>
2022-03-24 17:00:36 +0100

    Added new stuff for only client/server app

---
+++ AppWeb/app_padel/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+    
+  );
+}
+
+export default App;
